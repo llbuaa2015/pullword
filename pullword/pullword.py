aa79a588ec6b0a57601595ac6a1dbb2caa83940b
llbuaa2015 <llbuaa@163.com>
2017-04-06 11:08:16 +0800

    all

--- pullword/pullword.py
+++ pullword/pullword.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python
 # coding=utf-8
+
+from requests.packages.urllib3.connectionpool import HTTPConnectionPool
+
+
+def _make_request(self, conn, method, url, **kwargs):
+    response = self._old_make_request(conn, method, url, **kwargs)
+    sock = getattr(conn, 'sock', False)
+    if sock:
+        setattr(response, 'peer', sock.getpeername())
+    else:
+        setattr(response, 'peer', None)
+    return response
+
+HTTPConnectionPool._old_make_request = HTTPConnectionPool._make_request
+HTTPConnectionPool._make_request = _make_request
+
 import requests
 post_url = "http://api.pullword.com/post.php"
 
+
 class ServerError(Exception):
-     def __init__(self, value):
-         self.value = value
-     def __str__(self):
-         return repr(self.value)
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
 
 def split_word(words):
     words = words.split()
@@ -17,15 +36,16 @@ def split_word(words):
             continue
         words_list.append(i.split(":"))
     return words_list
-        
+
 
 def pullword(source="", threshold=0, debug=1):
-    payload = {"source":source.encode("utf8"), "param1":threshold, "param2":debug}
+    payload = {"source": source.encode(
+        "utf8"), "param1": threshold, "param2": debug}
     pw = requests.post(post_url, data=payload)
     print pw.url
+    print pw.raw._original_response.peer
+
     if pw.status_code != 200:
-        raise ServerError("server return %s"%pw.status_code)
+        raise ServerError("server return %s" % pw.status_code)
     print pw.content
     return split_word(pw.content)
-
-        
